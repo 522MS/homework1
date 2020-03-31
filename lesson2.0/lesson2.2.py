4ab542ad79d2a358035191b412b6ec5e504b7cd4
Shaykh Aliomarov <aliomarov.522@mail.ru>
2020-03-31 22:58:51 +0300

    Задание 2.2

---
+++ lesson2.0/lesson2.2.py
@@ -0,0 +1,11 @@
+my_list = []
+number_elements = int(input('Введите колличество элемонтов списка: '))
+
+while number_elements > 0:
+    my_list.append(input('Введите элемент списака: '))
+    number_elements -= 1
+
+print(my_list)
+
+
+# не смог поменять местами
