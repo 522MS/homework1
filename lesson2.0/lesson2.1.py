505347a6a4dc6f9fd56337dd36a91f230614677f
Shaykh Aliomarov <aliomarov.522@mail.ru>
2020-03-31 20:59:29 +0300

    Задание 2.1

---
+++ lesson2.0/lesson2.1.py
@@ -0,0 +1,9 @@
+name = 'Шейх'
+age = 23
+my_friends = ['Alexandr', 'Ramazan']
+my_address = {'city': 'Moscow', 'country': 'Russia'}
+
+my_list = [name, age, my_friends, my_address]
+
+for i in my_list:
+    print(i, type(i))
