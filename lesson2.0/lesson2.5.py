eece5d8ce5e146ef25ae983b7cfd441a7af2ef63
Shaykh Aliomarov <aliomarov.522@mail.ru>
2020-04-01 10:50:53 +0300

    Задание 2.6

--- lesson2.0/lesson2.5.py
+++ lesson2.0/lesson2.5.py
@@ -7,12 +7,11 @@ for element in my_list:
         i = my_list.index(number)
         my_list.insert(i+a, number)
         break
-    else:
-        if number > element:
+    elif number > element:
             j = my_list.index(element)
             my_list.insert(j, number)
             break
-        elif number < my_list[len(my_list) - 1]:
+    elif number < my_list[len(my_list) - 1]:
             my_list.append(number)
 
 print(my_list) 
