01f492eb88f9af95be85e896bd345216ef053cea
Shaykh Aliomarov <aliomarov.522@mail.ru>
2020-04-02 15:35:29 +0300

    Задание 3.2

---
+++ lesson3.0/lesson3.2.py
@@ -0,0 +1,5 @@
+def user_data(name, surname, year, city, email, phone):
+    print(name, surname, year, city, email, phone)
+
+user_data(name='Sheikh', surname='Aliomarov', year=1996, email='aliomarov.522@mail.ru', city='Moscow', phone='89998887766')
+
